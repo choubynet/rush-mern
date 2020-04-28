18d3f9203a8efc5ef370bcac7a447a9a6775dcfd
Sébastien CHOUBRAC <sebastien.choubrac@epitech.eu>
2020-04-28 15:05:11 +0200

    microbloggos login & register in progress

---
+++ MicroBloggos/client/src/actions/authActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { GET_ERRORS, SET_CURRENT_USER } from '../constants';
+
+export const registerUser = (userData, history) => dispatch => {
+    axios.post('http://localhost:5000/api/users/register', userData)
+        .then(res => history.push('/login'))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+export const getCurrentUser = () => dispatch => {
+    axios.get('http://localhost:5000/api/users/')
+        .then(res => dispatch(setCurrentUser(res.data)))
+}
+
+export const setCurrentUser = () => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: data
+    }
+}
